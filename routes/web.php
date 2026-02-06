f2bc379df4fba79ca77f866227419df155b0eaeb
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-07 00:19:20 +0530

    Revert "m"
    This reverts commit 16e919d0772ba38cc8670cb03f022c375962cc58.

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [RoutingController::class, 'root'])->name('root');
 Route::get('/log', [RoutingController::class, 'log'])->name('log');
-
-Route::get('/{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])->where('first', '^(?!build|assets|img|css|js|storage|vendor|favicon\.ico|robots\.txt|\.well-known).*$')->name('third');
-Route::get('/{first}/{second}', [RoutingController::class, 'secondLevel'])->where('first', '^(?!build|assets|img|css|js|storage|vendor|favicon\.ico|robots\.txt|\.well-known).*$')->name('second');
-Route::get('/{any}', [RoutingController::class, 'firstLevel'])->where('any', '^(?!build|assets|img|css|js|storage|vendor|favicon\.ico|robots\.txt|\.well-known).*$')->name('any');
-
 Route::get('/login', [RoutingController::class, 'login'])->name('login');
 
+Route::get('/{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])
+    ->where('first', '^(?!build|assets|img|css|js|storage|vendor|favicon\.ico|robots\.txt|\.well-known).*$')
+    ->name('third');
 
+Route::get('/{first}/{second}', [RoutingController::class, 'secondLevel'])
+    ->where('first', '^(?!build|assets|img|css|js|storage|vendor|favicon\.ico|robots\.txt|\.well-known).*$')
+    ->name('second');
+
+Route::get('/{any}', [RoutingController::class, 'firstLevel'])
+    ->where('any', '^(?!build|assets|img|css|js|storage|vendor|favicon\.ico|robots\.txt|\.well-known).*$')
+    ->name('any');
