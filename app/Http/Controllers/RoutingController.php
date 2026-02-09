e516d6196b0b3c4e7aa6fb3750b81ec0a797a382
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-10 02:15:59 +0530

    m

--- app/Http/Controllers/RoutingController.php
+++ app/Http/Controllers/RoutingController.php
@@ -19,7 +19,7 @@ class RoutingController extends Controller
 
     public function login()
     {
-        return view('auth-login-minimal');
+        return view('auth.auth-login-minimal');
     }
 
 
