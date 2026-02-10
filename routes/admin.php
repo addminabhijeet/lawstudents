07a87a967933e97337625de9ba5655db89f7c834
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-10 20:37:15 +0530

    m

--- routes/admin.php
+++ routes/admin.php
@@ -5,10 +5,7 @@ use App\Http\Controllers\RoutingController;
 
 Route::middleware(['web', 'auth:admin'])->prefix('admin')->group(function () {
 
-        Route::get('admin', [RoutingController::class, 'admin'])
-            ->name('admin.dashboard');
-        Route::get('admission', [RoutingController::class, 'admission'])
-            ->name('admin.admission');
-        Route::get('admissioncreate', [RoutingController::class, 'admissioncreate'])
-            ->name('admin.admissioncreate');
-    });
+    Route::get('admin', [RoutingController::class, 'admin'])->name('admin.dashboard');
+    Route::get('admission', [RoutingController::class, 'admission'])->name('admin.admission');
+    Route::get('admissioncreate', [RoutingController::class, 'admissioncreate'])->name('admin.admissioncreate');
+});
