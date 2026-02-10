c745cc8246893ebb153c678d258bb48d24e9b87b
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-11 00:32:12 +0530

    m

--- routes/admin.php
+++ routes/admin.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoutingController;
 
-Route::get('/',[RoutingController::class,'admin'])->name('admin.dashboard');
+Route::middleware(['web', 'auth:admin'])->prefix('admin')->group(function () {
 
-Route::get('admission',[RoutingController::class,'admission'])->name('admin.admission');
-
-Route::get('admissioncreate',[RoutingController::class,'admissioncreate'])->name('admin.admissioncreate');
+    Route::get('admin', [RoutingController::class, 'admin'])->name('admin.dashboard');
+    Route::get('admission', [RoutingController::class, 'admission'])->name('admin.admission');
+    Route::get('admissioncreate', [RoutingController::class, 'admissioncreate'])->name('admin.admissioncreate');
+});
--- routes/student.php
+++ routes/student.php
@@ -4,14 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoutingController;
 use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/',[RoutingController::class,'student'])->name('student.dashboard');
-
-Route::get('registerstu',[RoutingController::class,'registerstu'])->name('registerstu');
-
-Route::get('addstudent',[RoutingController::class,'addstudent'])->name('addstudent');
-
-Route::post('registerstusubmit',[LoginController::class,'registerstusubmit'])->name('admin.registerstusubmit');
-
-Route::get('liststudent',[RoutingController::class,'liststudent'])->name('liststudent');
-
-Route::post('studentregister',[LoginController::class,'studentregister'])->name('student.register');
+Route::middleware(['web', 'auth:student'])->prefix('student')->group(function () {
+    Route::get('student', [RoutingController::class, 'student'])->name('student.dashboard');
+    Route::get('registerstu', [RoutingController::class, 'registerstu'])->name('registerstu');
+    Route::get('addstudent', [RoutingController::class, 'addstudent'])->name('addstudent');
+    Route::post('registerstusubmit', [LoginController::class, 'registerstusubmit'])->name('admin.registerstusubmit');
+    Route::get('admin', [RoutingController::class, 'admin'])->name('admin');
+    Route::get('liststudent', [RoutingController::class, 'liststudent'])->name('liststudent');
+    Route::get('student', [RoutingController::class, 'student'])->name('student');
+    Route::get('admissioncreate', [RoutingController::class, 'admissioncreate'])->name('admin.admissioncreate');
+    Route::post('studentregister', [LoginController::class, 'studentregister'])->name('student.register');
+});
