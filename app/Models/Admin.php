2568d85b435c37c4993bd8b82a5b770345dab163
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-12 02:29:07 +0530

    m

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Hash;
 
 class Admin extends Authenticatable
 {
+    protected $table = 'admins';
+
+    protected $primaryKey = 'id';
+
     protected $fillable = [
         'name',
         'username',
@@ -17,12 +22,37 @@ class Admin extends Authenticatable
         'password',
     ];
 
-    protected function casts(): array
+    protected $casts = [
+        'created_at' => 'immutable_datetime',
+        'updated_at' => 'immutable_datetime',
+        'otp_expires_at' => 'datetime',
+    ];
+
+    /**
+     * Auto hash password like Student model
+     */
+    public function setPasswordAttribute($value)
     {
-        return [
-            'password' => 'hashed',
-            'created_at' => 'datetime',
-            'updated_at' => 'datetime',
-        ];
+        if ($value) {
+            $this->attributes['password'] = Hash::make($value);
+        }
+    }
+
+    public function generateOtp()
+    {
+        $otp = random_int(100000, 999999);
+
+        $this->otp = $otp;
+        $this->otp_expires_at = now()->addMinutes(10);
+        $this->save();
+
+        return $otp;
+    }
+
+    public function clearOtp()
+    {
+        $this->otp = null;
+        $this->otp_expires_at = null;
+        $this->save();
     }
 }
