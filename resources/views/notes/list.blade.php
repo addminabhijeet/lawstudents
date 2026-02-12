6cd4dac8bf70277f66ecbce815da1730a6f1ea5a
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-13 01:19:50 +0530

    m

--- resources/views/notes/list.blade.php
+++ resources/views/notes/list.blade.php
@@ -1087,21 +1087,6 @@
                 </div>
             </div>
         </div>
-        <!-- [ Footer ] start -->
-        <footer class="footer">
-            <p class="fs-11 text-muted fw-medium text-uppercase mb-0 copyright">
-                <span>Copyright ©</span>
-                <script>
-                    document.write(new Date().getFullYear());
-                </script>
-            </p>
-            <div class="d-flex align-items-center gap-4">
-                <a href="javascript:void(0);" class="fs-11 fw-semibold text-uppercase">Help</a>
-                <a href="javascript:void(0);" class="fs-11 fw-semibold text-uppercase">Terms</a>
-                <a href="javascript:void(0);" class="fs-11 fw-semibold text-uppercase">Privacy</a>
-            </div>
-        </footer>
-        <!-- [ Footer ] end -->
     </div>
     <!-- [ Content Area ] end -->
 </div>
