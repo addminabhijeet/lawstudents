90221cce3fe94f99fbcd2709eff731bd9fc99bb5
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-12 20:54:08 +0530

    m

--- resources/views/layouts/partials/admin/dashboard.blade.php
+++ resources/views/layouts/partials/admin/dashboard.blade.php
@@ -85,7 +85,7 @@
                         <ul class="nxl-submenu">
                             <li class="nxl-item"><a class="nxl-link" href="{{ route('admin.liststudent') }}">List
                                     Student</a></li>
-                            <li class="nxl-item"><a class="nxl-link" href="apps-email.html">Email</a></li>
+                            <li class="nxl-item"><a class="nxl-link" href="{{ route('admin.addstudent') }}">Add Student</a></li>
                             <li class="nxl-item"><a class="nxl-link" href="apps-tasks.html">Tasks</a></li>
                             <li class="nxl-item"><a class="nxl-link" href="apps-notes.html">Notes</a></li>
                             <li class="nxl-item"><a class="nxl-link" href="apps-storage.html">Storage</a></li>
