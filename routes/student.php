408737414dd86ef53c561db1b12b27d106dfcd62
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-13 05:05:48 +0530

    Revert "m"
    This reverts commit 112016fb15fc2e64fabb40b263c69e2256294e41.

--- routes/student.php
+++ routes/student.php
@@ -12,41 +12,41 @@ Route::middleware(['student.auth', 'auth:student'])
         Route::get('/', [RoutingControllerStu::class, 'student'])
             ->name('dashboard');
 
-        Route::get('list-student-subject', [RoutingControllerStu::class, 'listsubjectstu'])
-            ->name('listsubjectstu');
+        Route::get('list-student-subject', [RoutingControllerStu::class, 'listsubject'])
+            ->name('listsubject');
 
-        Route::get('list-student-course', [RoutingControllerStu::class, 'listcoursestu'])
-            ->name('listcoursestu');
+        Route::get('list-student-course', [RoutingControllerStu::class, 'listcourse'])
+            ->name('listcourse');
 
-        Route::get('add-student-student', [RoutingControllerStu::class, 'addstudentstu'])
-            ->name('addstudentstu');
+        Route::get('add-student-student', [RoutingControllerStu::class, 'addstudent'])
+            ->name('addstudent');
 
-        Route::get('list-student-student', [RoutingControllerStu::class, 'liststudentstu'])
-            ->name('liststudentstu');
+        Route::get('list-student-student', [RoutingControllerStu::class, 'liststudent'])
+            ->name('liststudent');
 
-        Route::get('add-student-admission', [RoutingControllerStu::class, 'addadmissionstu'])
-            ->name('addadmissionstu');
+        Route::get('add-student-admission', [RoutingControllerStu::class, 'addadmission'])
+            ->name('addadmission');
 
-        Route::get('list-student-admission', [RoutingControllerStu::class, 'listadmissionstu'])
-            ->name('listadmissionstu');
+        Route::get('list-student-admission', [RoutingControllerStu::class, 'listadmission'])
+            ->name('listadmission');
 
-        Route::get('add-student-payment', [RoutingControllerStu::class, 'addpaymentstu'])
-            ->name('addpaymentstu');
+        Route::get('add-student-payment', [RoutingControllerStu::class, 'addpayment'])
+            ->name('addpayment');
 
-        Route::get('view-student-payment', [RoutingControllerStu::class, 'viewpaymentstu'])
-            ->name('viewpaymentstu');
+        Route::get('view-student-payment', [RoutingControllerStu::class, 'viewpayment'])
+            ->name('viewpayment');
 
-        Route::get('list-student-payment', [RoutingControllerStu::class, 'listpaymentstu'])
-            ->name('listpaymentstu');
+        Route::get('list-student-payment', [RoutingControllerStu::class, 'listpayment'])
+            ->name('listpayment');
 
-        Route::get('list-student-notes', [RoutingControllerStu::class, 'listnotesstu'])
-            ->name('listnotesstu');
+        Route::get('list-student-notes', [RoutingControllerStu::class, 'listnotes'])
+            ->name('listnotes');
 
-        Route::get('list-student-subject', [RoutingControllerStu::class, 'listsubjectstu'])
-            ->name('listsubjectstu');
+        Route::get('list-student-subject', [RoutingControllerStu::class, 'listsubject'])
+            ->name('listsubject');
 
-        Route::get('list-student-course', [RoutingControllerStu::class, 'listcoursestu'])
-            ->name('listcoursestu');
+        Route::get('list-student-course', [RoutingControllerStu::class, 'listcourse'])
+            ->name('listcourse');
     });
 
 Route::get('/legacy-student', [RoutingControllerStu::class, 'student'])
