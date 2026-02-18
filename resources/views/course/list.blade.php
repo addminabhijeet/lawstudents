2b99d50c6e17547e0d768edc629073ce09647e86
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-18 23:37:25 +0530

    m

--- resources/views/course/list.blade.php
+++ resources/views/course/list.blade.php
@@ -370,15 +370,16 @@
                                             </p>
                                         </div>
 
-                                        <div class="d-flex align-items-center gap-2">
-                                            <span class="badge bg-primary text-truncate w-75 mb-1">
+                                        <div class="d-flex justify-content-between align-items-center">
+                                            <span class="badge bg-primary mb-1">
                                                 {{ $category->name }}
                                             </span>
 
-                                            <span class="fw-bold text-success">
+                                            <span class="fw-bold text-success ms-2">
                                                 ₹{{ $course->price }}
                                             </span>
                                         </div>
+
 
                                     </div>
                                 </div>
