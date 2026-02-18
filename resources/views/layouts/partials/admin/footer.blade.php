37a5511fb28639f2679d6f6da267c6065d413d62
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-18 20:58:40 +0530

    m

--- resources/views/layouts/partials/admin/footer.blade.php
+++ resources/views/layouts/partials/admin/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer mt-auto">
+<footer class="footer flex-fill">
     <p class="fs-11 text-muted fw-medium text-uppercase mb-0 copyright">
         <span>Copyright ©</span>
         <script>
