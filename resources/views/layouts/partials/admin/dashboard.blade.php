24246a7ece7419fe8a899b5d06132c607c128240
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-18 21:59:17 +0530

    m

--- resources/views/layouts/partials/admin/dashboard.blade.php
+++ resources/views/layouts/partials/admin/dashboard.blade.php
@@ -124,14 +124,6 @@
                     </div>
                 </a>
 
-                <div class="nxl-navigation-toggle">
-                    <a href="javascript:void(0);" id="menu-mini-button">
-                        <i class="feather-align-left"></i>
-                    </a>
-                    <a href="javascript:void(0);" id="menu-expend-button" style="display: none">
-                        <i class="feather-arrow-right"></i>
-                    </a>
-                </div>
 
             </div>
             <!--! [End] Header Left !-->
