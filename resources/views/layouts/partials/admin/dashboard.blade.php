fb23b2252d9631673cbc2798826ca6da3cac8215
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-18 21:56:29 +0530

    m

--- resources/views/layouts/partials/admin/dashboard.blade.php
+++ resources/views/layouts/partials/admin/dashboard.blade.php
@@ -115,7 +115,7 @@
         <div class="header-wrapper">
             <!--! [Start] Header Left !-->
             <div class="header-left d-flex align-items-center gap-4">
-                <!--! [Start] nxl-head-mobile-toggler !-->
+
                 <a href="javascript:void(0);" class="nxl-head-mobile-toggler" id="mobile-collapse">
                     <div class="hamburger hamburger--arrowturn">
                         <div class="hamburger-box">
@@ -123,8 +123,7 @@
                         </div>
                     </div>
                 </a>
-                <!--! [Start] nxl-head-mobile-toggler !-->
-                <!--! [Start] nxl-navigation-toggle !-->
+
                 <div class="nxl-navigation-toggle">
                     <a href="javascript:void(0);" id="menu-mini-button">
                         <i class="feather-align-left"></i>
@@ -133,25 +132,7 @@
                         <i class="feather-arrow-right"></i>
                     </a>
                 </div>
-                <!--! [End] nxl-navigation-toggle !-->
-                <!--! [Start] nxl-lavel-mega-menu-toggle !-->
-                <div class="nxl-lavel-mega-menu-toggle d-flex d-lg-none">
-                    <a href="javascript:void(0);" id="nxl-lavel-mega-menu-open">
-                        <i class="feather-align-left"></i>
-                    </a>
-                </div>
-                <!--! [End] nxl-lavel-mega-menu-toggle !-->
-                <!--! [Start] nxl-lavel-mega-menu !-->
-                <div class="nxl-drp-link nxl-lavel-mega-menu">
-                    <div class="nxl-lavel-mega-menu-toggle d-flex d-lg-none">
-                        <a href="javascript:void(0)" id="nxl-lavel-mega-menu-hide">
-                            <i class="feather-arrow-left me-2"></i>
-                            <span>Back</span>
-                        </a>
-                    </div>
-                    <!--! [End] nxl-lavel-mega-menu-wrapper !-->
-                </div>
-                <!--! [End] nxl-lavel-mega-menu !-->
+
             </div>
             <!--! [End] Header Left !-->
             <!--! [Start] Header Right !-->
