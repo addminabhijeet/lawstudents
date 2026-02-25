93d68e97f5d094285ca0b22f0a5909520f0ea236
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-25 17:43:23 +0530

    m

--- resources/views/student/add.blade.php
+++ resources/views/student/add.blade.php
@@ -133,17 +133,10 @@
                 </div>
             </div>
         </main>
-        <script src="assets/vendors/js/vendors.min.js"></script>
-        <!-- vendors.min.js {always must need to be top} -->
-        <script src="assets/vendors/js/lslstrength.min.js"></script>
-        <!--! END: Vendors JS !-->
-        <!--! BEGIN: Apps Init  !-->
-        <script src="assets/js/common-init.min.js"></script>
-        <!--! END: Apps Init !-->
-        <!--! BEGIN: Theme Customizer  !-->
-        <script src="assets/js/theme-customizer-init.min.js"></script>
-        <!--! END: Theme Customizer !-->
-        <!-- [ Main Content ] end -->
+    <script src="{{ asset('assets/vendors/js/vendors.min.js') }}"></script>
+    <script src="{{ asset('assets/vendors/js/lslstrength.min.js') }}"></script>
+    <script src="{{ asset('assets/js/common-init.min.js') }}"></script>
+    <script src="{{ asset('assets/js/theme-customizer-init.min.js') }}"></script>
     </div>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
