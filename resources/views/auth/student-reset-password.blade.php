0618633a42dba7577a0d7dbe315ebb1c7f2d1886
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-25 17:34:02 +0530

    m

--- resources/views/auth/student-reset-password.blade.php
+++ resources/views/auth/student-reset-password.blade.php
@@ -108,8 +108,8 @@
                                     <input type="password" name="password_confirmation" class="form-control password"
                                         id="confirmPassword" placeholder="Confirm Password" required>
                                     <div class="input-group-text border-start bg-gray-2 c-pointer show-pass"
-                                        data-bs-toggle="tooltip" title="Show/Hide Password">
-                                        <i></i>
+                                        id="toggleConfirmPassword" data-bs-toggle="tooltip" title="Show/Hide Password">
+                                        <i>👁️</i>
                                     </div>
                                 </div>
                             </div>
