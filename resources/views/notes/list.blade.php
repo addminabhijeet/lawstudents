6a95cebcf660a8623278e7ec30e6e6b75d71f2bf
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-25 22:07:44 +0530

    m

--- resources/views/notes/list.blade.php
+++ resources/views/notes/list.blade.php
@@ -390,7 +390,7 @@
                                                     class="btn btn-info w-100 mb-2 view-note-btn"
                                                     data-id="{{ $note->id }}" data-title="{{ $note->title }}"
                                                     data-description="{{ $note->description }}"
-                                                    data-pdf="{{ asset('storage/' . $note->pdf_path) }}">
+                                                    data-pdf="{{ asset('storage/' . $note->file_path) }}">
                                                     <i class="feather-eye me-2"></i>
                                                     <span>View Note</span>
                                                 </a>
