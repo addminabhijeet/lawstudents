83bb26ffd8b8c9c50976534d7809cd72809f52ce
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-25 23:08:21 +0530

    m

--- app/Http/Controllers/Frontend/FreeNotesController.php
+++ app/Http/Controllers/Frontend/FreeNotesController.php
@@ -4,11 +4,59 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\View\View;
+use App\Models\Course;
+use App\Models\Category;
 
 class FreeNotesController extends Controller
 {
     public function __invoke(): View
     {
-        return view('notes.notes');
+        $categories = Category::whereHas('courses.notes', function ($query) {
+            $query->where('visibility', 'free')
+                ->where('status', 1);
+        })
+            ->with([
+                'courses' => function ($query) {
+                    $query->where('is_free', 1)
+                        ->whereHas('notes', function ($q) {
+                            $q->where('visibility', 'free')
+                                ->where('status', 1);
+                        })
+                        ->with(['notes' => function ($q) {
+                            $q->where('visibility', 'free')
+                                ->where('status', 1);
+                        }]);
+                },
+                'children' => function ($query) {
+                    $query->whereHas('courses.notes', function ($q) {
+                        $q->where('visibility', 'free')
+                            ->where('status', 1);
+                    })
+                        ->with([
+                            'courses' => function ($q) {
+                                $q->where('is_free', 1)
+                                    ->whereHas('notes', function ($n) {
+                                        $n->where('visibility', 'free')
+                                            ->where('status', 1);
+                                    })
+                                    ->with(['notes' => function ($n) {
+                                        $n->where('visibility', 'free')
+                                            ->where('status', 1);
+                                    }]);
+                            }
+                        ]);
+                }
+            ])
+            ->get();
+
+        $courses = Course::where('status', 1)
+            ->where('is_free', 1)
+            ->whereHas('notes', function ($query) {
+                $query->where('visibility', 'free')
+                    ->where('status', 1);
+            })
+            ->get();
+
+        return view('notes.notes', compact('categories', 'courses'));
     }
 }
--- resources/views/notes/notes.blade.php
+++ resources/views/notes/notes.blade.php
@@ -1,322 +1,82 @@
 @extends('layouts.landing', ['title' => 'Lawsy || Criminal Law || Free Notes'])
 
 @section('content')
-  <!--===== WELCOME STARTS =======-->
-  <div class="welcome-inner-section-area" style="background-image: url(/img/bacground/inner-bg.png); background-position: center; background-repeat: no-repeat; background-size: cover;">
-    <img src="/img/elements/elementor40.png" alt="" class="elementor40 keyframe3 d-lg-block d-none">
-    <div class="container">
-    <div class="row">
-      <div class="col-lg-3 m-auto">
-      <div class="welcome-inner-header text-center">
-        <h1>Free Notes</h1>
-        <a href="">Home <span><i class="fa-light fa-angle-right"></i></span> Free Notes</a>
-        <img src="/img/elements/elementor20.png" alt="">
-      </div>
-      </div>
+    <!--===== WELCOME STARTS =======-->
+    <div class="welcome-inner-section-area"
+        style="background-image: url(/img/bacground/inner-bg.png); background-position: center; background-repeat: no-repeat; background-size: cover;">
+        <img src="/img/elements/elementor40.png" alt="" class="elementor40 keyframe3 d-lg-block d-none">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 m-auto">
+                    <div class="welcome-inner-header text-center">
+                        <h1>Free Notes</h1>
+                        <a href="">Home <span><i class="fa-light fa-angle-right"></i></span> Free Notes</a>
+                        <img src="/img/elements/elementor20.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--===== WELCOME ENDS =======-->
+
+    <!--===== BLOG STARTS =======-->
+    <div class="blog1-section-area sp3">
+        <div class="container">
+            <div class="row">
+                @foreach ($courses as $course)
+                    @foreach ($course->notes as $note)
+                        <div class="col-lg-4 col-md-6">
+                            <div class="blog-boxarea">
+                                <div class="blog-images">
+                                    <img src="{{ asset('img/images/blog-img1.png') }}" alt="">
+                                </div>
+
+                                <div class="blog-all-textarea">
+
+                                    <a href="{{ route('admin.viewnote', $note->id) }}" target="_blank">
+                                        {{ $note->title }}
+                                    </a>
+
+                                    <p>
+                                        Course: {{ $course->title }}
+                                    </p>
+
+                                    <p>
+                                        Size: {{ $note->formatted_size }}
+                                    </p>
+
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                @endforeach
+            </div>
+
+            <div class="col-lg-12 m-auto">
+                <div class="pagination-area">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Previous">
+                                    <span aria-hidden="true"><i class="fa-regular fa-angle-left"></i></span>
+                                </a>
+                            </li>
+                            <li class="page-item"><a class="page-link active" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">...</a></li>
+                            <li class="page-item"><a class="page-link" href="#">12</a></li>
+                            <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Next">
+                                    <span aria-hidden="true"><i class="fa-regular fa-angle-right"></i></span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
     </div>
     </div>
-  </div>
-  <!--===== WELCOME ENDS =======-->
-
-  <!--===== BLOG STARTS =======-->
-  <div class="blog1-section-area sp3">
-    <div class="container">
-    <div class="row">
-      <div class="col-lg-4 col-md-6">
-      <div class="blog-boxarea">
-        <div class="blog-images">
-        <img src="/img/images/blog-img1.png" alt="">
-        <div class="date-img">
-          <img src="/img/images/date7.png" alt="">
-        </div>
-        </div>
-        <div class="blog-all-textarea">
-        <div class="blog-text-area">
-          <div class="blog-name-area">
-          <img src="/img/icons/contact-img1.svg" alt="">
-          <a href="#">
-            <p>Henry Nicolls</p>
-          </a>
-          </div>
-          <div class="blog-name-area">
-          <img src="/img/icons/tax-img1.svg" alt="">
-          <a href="#">
-            <p>Tax Lawyer</p>
-          </a>
-          </div>
-        </div>
-        <a href="">Experience Matters: Your Tax Your & Resolution Starts Here</a>
-        <p>As a small business owner, you're well aware of the numerous financial responsibilities.</p>
-        <a href="" class="readmore">Read More <i class="fa-light fa-arrow-right"></i></a>
-        </div>
-      </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6">
-      <div class="blog-boxarea">
-        <div class="blog-images">
-        <img src="/img/images/blog-img2.png" alt="">
-        <div class="date-img">
-          <img src="/img/images/date8.png" alt="">
-        </div>
-        </div>
-        <div class="blog-all-textarea">
-        <div class="blog-text-area">
-          <div class="blog-name-area">
-          <img src="/img/icons/contact-img1.svg" alt="">
-          <a href="#">
-            <p>Henry Nicolls</p>
-          </a>
-          </div>
-          <div class="blog-name-area">
-          <img src="/img/icons/tax-img1.svg" alt="">
-          <a href="#">
-            <p>Tax Lawyer</p>
-          </a>
-          </div>
-        </div>
-        <a href="">Navigating the Tax Maze: Your Path to Financial Peace of Mind.</a>
-        <p>As a small business owner, you're well aware of the numerous financial responsibilities.</p>
-        <a href="" class="readmore">Read More <i class="fa-light fa-arrow-right"></i></a>
-        </div>
-      </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6">
-      <div class="blog-boxarea">
-        <div class="blog-images">
-        <img src="/img/images/blog-img3.png" alt="">
-        <div class="date-img">
-          <img src="/img/images/date9.png" alt="">
-        </div>
-        </div>
-        <div class="blog-all-textarea">
-        <div class="blog-text-area">
-          <div class="blog-name-area">
-          <img src="/img/icons/contact-img1.svg" alt="">
-          <a href="#">
-            <p>Henry Nicolls</p>
-          </a>
-          </div>
-          <div class="blog-name-area">
-          <img src="/img/icons/tax-img1.svg" alt="">
-          <a href="#">
-            <p>Tax Lawyer</p>
-          </a>
-          </div>
-        </div>
-        <a href="">Your Trusted Tax Attorney: Let's Solve Tax Issues Together</a>
-        <p>As a small business owner, you're well aware of the numerous financial responsibilities.</p>
-        <a href="" class="readmore">Read More <i class="fa-light fa-arrow-right"></i></a>
-        </div>
-      </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6">
-      <div class="blog-boxarea">
-        <div class="blog-images">
-        <img src="/img/images/blog3-inner-img1.png" alt="">
-        <div class="date-img">
-          <img src="/img/images/date10.png" alt="">
-        </div>
-        </div>
-        <div class="blog-all-textarea">
-        <div class="blog-text-area">
-          <div class="blog-name-area">
-          <img src="/img/icons/contact-img1.svg" alt="">
-          <a href="#">
-            <p>Henry Nicolls</p>
-          </a>
-          </div>
-          <div class="blog-name-area">
-          <img src="/img/icons/tax-img1.svg" alt="">
-          <a href="#">
-            <p>Tax Lawyer</p>
-          </a>
-          </div>
-        </div>
-        <a href="">Champions in Defense: Advocating for Your Freedom and Justice</a>
-        <p>As a small business owner, you're well aware of the numerous financial responsibilities.</p>
-        <a href="" class="readmore">Read More <i class="fa-light fa-arrow-right"></i></a>
-        </div>
-      </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6">
-      <div class="blog-boxarea">
-        <div class="blog-images">
-        <img src="/img/images/blog3-inner-img2.png" alt="">
-        <div class="date-img">
-          <img src="/img/images/date11.png" alt="">
-        </div>
-        </div>
-        <div class="blog-all-textarea">
-        <div class="blog-text-area">
-          <div class="blog-name-area">
-          <img src="/img/icons/contact-img1.svg" alt="">
-          <a href="#">
-            <p>Henry Nicolls</p>
-          </a>
-          </div>
-          <div class="blog-name-area">
-          <img src="/img/icons/tax-img1.svg" alt="">
-          <a href="#">
-            <p>Tax Lawyer</p>
-          </a>
-          </div>
-        </div>
-        <a href="">Expert Defense Counsel: Your Law Lawyer Trusted Legal Guardians</a>
-        <p>As a small business owner, you're well aware of the numerous financial responsibilities.</p>
-        <a href="" class="readmore">Read More <i class="fa-light fa-arrow-right"></i></a>
-        </div>
-      </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6">
-      <div class="blog-boxarea">
-        <div class="blog-images">
-        <img src="/img/images/blog3-inner-img3.png" alt="">
-        <div class="date-img">
-          <img src="/img/images/date12.png" alt="">
-        </div>
-        </div>
-        <div class="blog-all-textarea">
-        <div class="blog-text-area">
-          <div class="blog-name-area">
-          <img src="/img/icons/contact-img1.svg" alt="">
-          <a href="#">
-            <p>Henry Nicolls</p>
-          </a>
-          </div>
-          <div class="blog-name-area">
-          <img src="/img/icons/tax-img1.svg" alt="">
-          <a href="#">
-            <p>Tax Lawyer</p>
-          </a>
-          </div>
-        </div>
-        <a href="">Unyielding Defense Advocates: Crafting Your Strongest Case</a>
-        <p>As a small business owner, you're well aware of the numerous financial responsibilities.</p>
-        <a href="" class="readmore">Read More <i class="fa-light fa-arrow-right"></i></a>
-        </div>
-      </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6">
-      <div class="blog-boxarea">
-        <div class="blog-images">
-        <img src="/img/images/blog3-inner-img4.png" alt="">
-        <div class="date-img">
-          <img src="/img/images/date13.png" alt="">
-        </div>
-        </div>
-        <div class="blog-all-textarea">
-        <div class="blog-text-area">
-          <div class="blog-name-area">
-          <img src="/img/icons/contact-img1.svg" alt="">
-          <a href="#">
-            <p>Henry Nicolls</p>
-          </a>
-          </div>
-          <div class="blog-name-area">
-          <img src="/img/icons/tax-img1.svg" alt="">
-          <a href="#">
-            <p>Tax Lawyer</p>
-          </a>
-          </div>
-        </div>
-        <a href="">Strategic Defense Solutions: Law Navigating Legal Complexities</a>
-        <p>As a small business owner, you're well aware of the numerous financial responsibilities.</p>
-        <a href="" class="readmore">Read More <i class="fa-light fa-arrow-right"></i></a>
-        </div>
-      </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6">
-      <div class="blog-boxarea">
-        <div class="blog-images">
-        <img src="/img/images/blog3-inner-img5.png" alt="">
-        <div class="date-img">
-          <img src="/img/images/date14.png" alt="">
-        </div>
-        </div>
-        <div class="blog-all-textarea">
-        <div class="blog-text-area">
-          <div class="blog-name-area">
-          <img src="/img/icons/contact-img1.svg" alt="">
-          <a href="#">
-            <p>Henry Nicolls</p>
-          </a>
-          </div>
-          <div class="blog-name-area">
-          <img src="/img/icons/tax-img1.svg" alt="">
-          <a href="#">
-            <p>Tax Lawyer</p>
-          </a>
-          </div>
-        </div>
-        <a href="">Defense Excellence: Protecting Law Clients with Precision and Skill</a>
-        <p>As a small business owner, you're well aware of the numerous financial responsibilities.</p>
-        <a href="" class="readmore">Read More <i class="fa-light fa-arrow-right"></i></a>
-        </div>
-      </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6">
-      <div class="blog-boxarea">
-        <div class="blog-images">
-        <img src="/img/images/blog3-inner-img6.png" alt="">
-        <div class="date-img">
-          <img src="/img/images/date15.png" alt="">
-        </div>
-        </div>
-        <div class="blog-all-textarea">
-        <div class="blog-text-area">
-          <div class="blog-name-area">
-          <img src="/img/icons/contact-img1.svg" alt="">
-          <a href="#">
-            <p>Henry Nicolls</p>
-          </a>
-          </div>
-          <div class="blog-name-area">
-          <img src="/img/icons/tax-img1.svg" alt="">
-          <a href="#">
-            <p>Tax Lawyer</p>
-          </a>
-          </div>
-        </div>
-        <a href="">Fortifying Your Defense: Proficient Lawyers, Powerful Strategies</a>
-        <p>As a small business owner, you're well aware of the numerous financial responsibilities.</p>
-        <a href="" class="readmore">Read More <i class="fa-light fa-arrow-right"></i></a>
-        </div>
-      </div>
-      </div>
-
-      <div class="col-lg-12 m-auto">
-      <div class="pagination-area">
-        <nav aria-label="Page navigation example">
-        <ul class="pagination">
-          <li class="page-item">
-          <a class="page-link" href="#" aria-label="Previous">
-            <span aria-hidden="true"><i class="fa-regular fa-angle-left"></i></span>
-          </a>
-          </li>
-          <li class="page-item"><a class="page-link active" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
-          <li class="page-item"><a class="page-link" href="#">...</a></li>
-          <li class="page-item"><a class="page-link" href="#">12</a></li>
-          <li class="page-item">
-          <a class="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true"><i class="fa-regular fa-angle-right"></i></span>
-          </a>
-          </li>
-        </ul>
-        </nav>
-      </div>
-      </div>
-    </div>
-    </div>
-  </div>
-  <!--===== BLOG ENDS =======-->
+    <!--===== BLOG ENDS =======-->
 @endsection
