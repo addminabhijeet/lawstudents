7f4bf087d59e7f27185967a25c3261a66c482b24
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-02-27 23:52:36 +0530

    m

--- resources/views/payment/list.blade.php
+++ resources/views/payment/list.blade.php
@@ -294,6 +294,11 @@
                                                             <i class="feather feather-eye"></i>
                                                         </a>
 
+                                                        <a href="{{ route('admin.viewidcard', $payment->id) }}"
+                                                            class="avatar-text avatar-md">
+                                                            <i class="fas fa-id-card"></i>
+                                                        </a>
+
                                                         <a href="{{ route('admin.editpayment', $payment->id) }}"
                                                             class="avatar-text avatar-md">
                                                             <i class="feather feather-edit"></i>
