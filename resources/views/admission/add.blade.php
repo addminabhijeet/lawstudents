b6ee34d00bd2dc5a3f0e40c202b2f0581038440e
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-03-02 19:31:01 +0530

    m

--- resources/views/admission/add.blade.php
+++ resources/views/admission/add.blade.php
@@ -1,27 +1,43 @@
 @include('layouts.partials.admin.dashboard')
-<main class="container-fluid py-4 bg-light">
-    <div class="container">
-
-        <div class="d-flex justify-content-between align-items-center mb-4">
-            <div>
-                <h4 class="fw-bold mb-1">Create Admission Proposal</h4>
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb small mb-0">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Create</li>
-                    </ol>
-                </nav>
+<main class="nxl-container">
+    <div class="nxl-content">
+        <div class="page-header">
+            <div class="page-header-left d-flex align-items-center">
+                <div class="page-header-title">
+                    <h5 class="m-b-10">Proposal</h5>
+                </div>
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item">Create</li>
+                </ul>
             </div>
-            <div class="d-flex gap-2">
-                <a href="javascript:void(0);" class="btn btn-outline-primary">
-                    <i class="feather-save me-1"></i> Save
-                </a>
-                <a href="javascript:void(0);" class="btn btn-primary">
-                    <i class="feather-send me-1"></i> Save & Send
-                </a>
+            <div class="page-header-right ms-auto">
+                <div class="page-header-right-items">
+                    <div class="d-flex d-md-none">
+                        <a href="javascript:void(0)" class="page-header-right-close-toggle">
+                            <i class="feather-arrow-left me-2"></i>
+                            <span>Back</span>
+                        </a>
+                    </div>
+                    <div class="d-flex align-items-center gap-2 page-header-right-items-wrapper">
+                        <a href="javascript:void(0);" class="btn btn-light-brand" data-bs-toggle="offcanvas"
+                            data-bs-target="#proposalSent">
+                            <i class="feather-layers me-2"></i>
+                            <span>Save & Send</span>
+                        </a>
+                        <a href="javascript:void(0);" class="btn btn-primary successAlertMessage">
+                            <i class="feather-save me-2"></i>
+                            <span>Save</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="d-md-none d-flex align-items-center">
+                    <a href="javascript:void(0)" class="page-header-right-open-toggle">
+                        <i class="feather-align-right fs-20"></i>
+                    </a>
+                </div>
             </div>
         </div>
-
         <form action="{{ route('admin.registeradmsubmit') }}" method="POST" enctype="multipart/form-data">
             @csrf
 
