08134055e97b79a5f059461fcea52770c885e33b
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-03-05 03:38:46 +0530

    m

--- app/Http/Controllers/Frontend/CourseController.php
+++ app/Http/Controllers/Frontend/CourseController.php
@@ -4,11 +4,59 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\View\View;
+use App\Models\Category;
+use App\Models\Course;
 
 class CourseController extends Controller
 {
     public function __invoke(): View
     {
-        return view('course.course');
+        $categories = Category::whereHas('courses.notes', function ($query) {
+            $query->where('visibility', 'free')
+                ->where('status', 1);
+        })
+            ->with([
+                'courses' => function ($query) {
+                    $query->where('is_free', 1)
+                        ->whereHas('notes', function ($q) {
+                            $q->where('visibility', 'free')
+                                ->where('status', 1);
+                        })
+                        ->with(['notes' => function ($q) {
+                            $q->where('visibility', 'free')
+                                ->where('status', 1);
+                        }]);
+                },
+                'children' => function ($query) {
+                    $query->whereHas('courses.notes', function ($q) {
+                        $q->where('visibility', 'free')
+                            ->where('status', 1);
+                    })
+                        ->with([
+                            'courses' => function ($q) {
+                                $q->where('is_free', 1)
+                                    ->whereHas('notes', function ($n) {
+                                        $n->where('visibility', 'free')
+                                            ->where('status', 1);
+                                    })
+                                    ->with(['notes' => function ($n) {
+                                        $n->where('visibility', 'free')
+                                            ->where('status', 1);
+                                    }]);
+                            }
+                        ]);
+                }
+            ])
+            ->get();
+
+        $courses = Course::where('status', 1)
+            ->where('is_free', 1)
+            ->whereHas('notes', function ($query) {
+                $query->where('visibility', 'free')
+                    ->where('status', 1);
+            })
+            ->get();
+
+        return view('courses.courses', compact('categories', 'courses'));
     }
 }
