209e9dbf8f30a0e23d36cc56ab0ca4e224bc7a85
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-03-10 21:27:25 +0530

    m

--- resources/views/student/add.blade.php
+++ resources/views/student/add.blade.php
@@ -42,20 +42,20 @@
         </div>
         <!-- [ page-header ] end -->
         <!-- [ Main Content ] start -->
-        <main class="auth-minimal-wrapper">
-            <div class="auth-minimal-inner">
-                <div class="minimal-card-wrapper">
-                    <div class="card mb-4 mt-5 mx-4 mx-sm-0 position-relative">
-                        <div
-                            class="wd-50 bg-white p-2 rounded-circle shadow-lg position-absolute translate-middle top-0 start-50">
-                            <img src="assets/images/logo-abbr.png" alt="" class="img-fluid">
-                        </div>
-                        <div class="card-body p-sm-5">
-                            <h2 class="fs-20 fw-bolder mb-4">Register</h2>
-                            <h4 class="fs-13 fw-bold mb-2">Manage all your Duralux crm</h4>
-                            <p class="fs-12 fw-medium text-muted">Let's get you all setup, so you can verify your
-                                personal
-                                account and begine setting up your profile.</p>
+        <div class="main-content">
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="card stretch stretch-full mt-5">
+                        <div class="card-body p-4">
+
+                            <div class="text-center mb-4">
+                                <img src="assets/images/logo-abbr.png" width="50" class="mb-3">
+                                <h2 class="fs-20 fw-bolder">Register</h2>
+                                <p class="fs-12 text-muted">
+                                    Let's get you all setup so you can verify your personal account
+                                    and begin setting up your profile.
+                                </p>
+                            </div>
 
                             {{-- Display Login Errors and Validation Messages --}}
                             @if ($errors->any())
@@ -65,56 +65,60 @@
                                             <li>{{ $error }}</li>
                                         @endforeach
                                     </ul>
-                                    <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                        aria-label="Close"></button>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
                                 </div>
                             @endif
 
                             @if (session('error'))
-                                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                <div class="alert alert-danger alert-dismissible fade show">
                                     {{ session('error') }}
-                                    <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                        aria-label="Close"></button>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
                                 </div>
                             @endif
 
                             @if (session('success'))
-                                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                <div class="alert alert-success alert-dismissible fade show">
                                     {{ session('success') }}
-                                    <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                        aria-label="Close"></button>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
                                 </div>
                             @endif
 
-                            <form action="{{ route('admin.registerstusubmit') }}" method="POST"
-                                class="w-100 mt-4 pt-2">
+                            <form action="{{ route('admin.registerstusubmit') }}" method="POST" class="mt-4">
                                 @csrf
-                                <div class="mb-4">
+
+                                <div class="mb-3">
                                     <input type="text" name="name" class="form-control" placeholder="Full Name"
                                         required>
                                 </div>
-                                <div class="mb-4">
+
+                                <div class="mb-3">
                                     <input type="text" name="username" class="form-control" placeholder="Username"
                                         required>
                                 </div>
-                                <div class="mb-4">
+
+                                <div class="mb-3">
                                     <input type="email" name="email" class="form-control" placeholder="Email"
                                         required>
                                 </div>
-                                <div class="mb-4 generate-pass">
+
+                                <div class="mb-3 generate-pass">
                                     <div class="input-group field">
                                         <input type="password" name="password" class="form-control password"
                                             id="newPassword" placeholder="Password" required>
+
                                         <div class="input-group-text border-start bg-gray-2 c-pointer show-pass"
-                                            data-bs-toggle="tooltip" title="Show/Hide Password"><i></i></div>
+                                            data-bs-toggle="tooltip" title="Show/Hide Password">
+                                            <i></i>
+                                        </div>
                                     </div>
                                 </div>
 
-                                <div class="mb-4">
+                                <div class="mb-3">
                                     <div class="input-group field">
                                         <input type="password" name="password_confirmation"
                                             class="form-control password" id="confirmPassword"
                                             placeholder="Confirm Password" required>
+
                                         <div class="input-group-text border-start bg-gray-2 c-pointer show-pass"
                                             id="toggleConfirmPassword" data-bs-toggle="tooltip"
                                             title="Show/Hide Password">
@@ -123,16 +127,19 @@
                                     </div>
                                 </div>
 
-                                <div class="mt-5">
-                                    <button type="submit" class="btn btn-lg btn-primary w-100">Create
-                                        Account</button>
+                                <div class="mt-4">
+                                    <button type="submit" class="btn btn-primary w-100">
+                                        Create Account
+                                    </button>
                                 </div>
+
                             </form>
+
                         </div>
                     </div>
                 </div>
             </div>
-        </main>
+        </div>
         <script src="{{ asset('assets/vendors/js/vendors.min.js') }}"></script>
         <script src="{{ asset('assets/vendors/js/lslstrength.min.js') }}"></script>
         <script src="{{ asset('assets/js/common-init.min.js') }}"></script>
