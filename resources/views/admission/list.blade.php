0fcc8493032cc613f17863d5008244e0e63c2edf
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-03-11 04:42:48 +0530

    m

--- resources/views/admission/list.blade.php
+++ resources/views/admission/list.blade.php
@@ -6,11 +6,11 @@
         <div class="page-header">
             <div class="page-header-left d-flex align-items-center">
                 <div class="page-header-title">
-                    <h5 class="m-b-10">Payment</h5>
+                    <h5 class="m-b-10">Admin</h5>
                 </div>
                 <ul class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                    <li class="breadcrumb-item">Payment</li>
+                    <li class="breadcrumb-item">Applications</li>
+                    <li class="breadcrumb-item">Student Admission List</li>
                 </ul>
             </div>
             <div class="page-header-right ms-auto">
