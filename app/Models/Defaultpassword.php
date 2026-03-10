ed635c5ebc6fed94822b76893de3d32a77a2b26f
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-03-11 02:54:20 +0530

    m

--- app/Models/Defaultpassword.php
+++ app/Models/Defaultpassword.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Defaultpassword extends Model
 {
+    protected $table = 'defaultpassword';
     protected $fillable = [
         'defaultpassword'
     ];
