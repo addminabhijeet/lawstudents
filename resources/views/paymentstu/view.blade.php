27918db4e09269b1a1df89eb6ab991a5f477ff0f
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-03-11 22:43:50 +0530

    m

--- resources/views/paymentstu/view.blade.php
+++ resources/views/paymentstu/view.blade.php
@@ -1,4 +1,4 @@
-@include('layouts.partials.student.dashboard')
+@include('layouts.partials.admin.dashboard')
 <main class="nxl-container">
     <!-- main containts -->
     <div class="nxl-content">
@@ -6,185 +6,12 @@
         <div class="page-header">
             <div class="page-header-left d-flex align-items-center">
                 <div class="page-header-title">
-                    <h5 class="m-b-10">Invoice</h5>
+                    <h5 class="m-b-10">Student</h5>
                 </div>
                 <ul class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item">Applications</li>
                     <li class="breadcrumb-item">View</li>
                 </ul>
-            </div>
-            <div class="page-header-right ms-auto">
-                <div class="page-header-right-items">
-                    <div class="d-flex d-md-none">
-                        <a href="javascript:void(0)" class="page-header-right-close-toggle">
-                            <i class="feather-arrow-left me-2"></i>
-                            <span>Back</span>
-                        </a>
-                    </div>
-                    <div class="d-flex align-items-center gap-2 page-header-right-items-wrapper">
-                        <a href="javascript:void(0);" class="btn btn-icon btn-light-brand" data-bs-toggle="collapse"
-                            data-bs-target="#collapseOne">
-                            <i class="feather-bar-chart"></i>
-                        </a>
-                        <div class="dropdown">
-                            <a class="btn btn-icon btn-light-brand" data-bs-toggle="dropdown" data-bs-offset="0, 10"
-                                data-bs-auto-close="outside">
-                                <i class="feather-filter"></i>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-end">
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="feather-eye me-3"></i>
-                                    <span>All</span>
-                                </a>
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="feather-send me-3"></i>
-                                    <span>Sent</span>
-                                </a>
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="feather-book-open me-3"></i>
-                                    <span>Open</span>
-                                </a>
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="feather-archive me-3"></i>
-                                    <span>Draft</span>
-                                </a>
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="feather-bell me-3"></i>
-                                    <span>Revised</span>
-                                </a>
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="feather-shield-off me-3"></i>
-                                    <span>Declined</span>
-                                </a>
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="feather-check me-3"></i>
-                                    <span>Accepted</span>
-                                </a>
-                                <div class="dropdown-divider"></div>
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="feather-briefcase me-3"></i>
-                                    <span>Leads</span>
-                                </a>
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="feather-wifi-off me-3"></i>
-                                    <span>Expired</span>
-                                </a>
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="feather-users me-3"></i>
-                                    <span>Customers</span>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="dropdown">
-                            <a class="btn btn-icon btn-light-brand" data-bs-toggle="dropdown" data-bs-offset="0, 10"
-                                data-bs-auto-close="outside">
-                                <i class="feather-paperclip"></i>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-end">
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="bi bi-filetype-pdf me-3"></i>
-                                    <span>PDF</span>
-                                </a>
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="bi bi-filetype-csv me-3"></i>
-                                    <span>CSV</span>
-                                </a>
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="bi bi-filetype-xml me-3"></i>
-                                    <span>XML</span>
-                                </a>
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="bi bi-filetype-txt me-3"></i>
-                                    <span>Text</span>
-                                </a>
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="bi bi-filetype-exe me-3"></i>
-                                    <span>Excel</span>
-                                </a>
-                                <div class="dropdown-divider"></div>
-                                <a href="javascript:void(0);" class="dropdown-item">
-                                    <i class="bi bi-printer me-3"></i>
-                                    <span>Print</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="d-md-none d-flex align-items-center">
-                    <a href="javascript:void(0)" class="page-header-right-open-toggle">
-                        <i class="feather-align-right fs-20"></i>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <div id="collapseOne" class="accordion-collapse collapse page-header-collapse">
-            <div class="accordion-body pb-2">
-                <div class="row">
-                    <div class="col-lg-3">
-                        <div class="card stretch stretch-full">
-                            <div class="card-body">
-                                <div class="d-flex align-items-center justify-content-between">
-                                    <a href="javascript:void(0);" class="fw-bold d-block">
-                                        <span class="d-block">Paid</span>
-                                        <span class="fs-20 fw-bold d-block">78/100</span>
-                                    </a>
-                                    <div class="badge bg-soft-success text-success">
-                                        <i class="feather-arrow-up fs-10 me-1"></i>
-                                        <span>36.85%</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="card stretch stretch-full">
-                            <div class="card-body">
-                                <div class="d-flex align-items-center justify-content-between">
-                                    <a href="javascript:void(0);" class="fw-bold d-block">
-                                        <span class="d-block">Unpaid</span>
-                                        <span class="fs-20 fw-bold d-block">38/50</span>
-                                    </a>
-                                    <div class="badge bg-soft-danger text-danger">
-                                        <i class="feather-arrow-down fs-10 me-1"></i>
-                                        <span>23.45%</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="card stretch stretch-full">
-                            <div class="card-body">
-                                <div class="d-flex align-items-center justify-content-between">
-                                    <a href="javascript:void(0);" class="fw-bold d-block">
-                                        <span class="d-block">Overdue</span>
-                                        <span class="fs-20 fw-bold d-block">15/30</span>
-                                    </a>
-                                    <div class="badge bg-soft-success text-success">
-                                        <i class="feather-arrow-up fs-10 me-1"></i>
-                                        <span>25.44%</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3">
-                        <div class="card stretch stretch-full">
-                            <div class="card-body">
-                                <div class="d-flex align-items-center justify-content-between">
-                                    <a href="javascript:void(0);" class="fw-bold d-block">
-                                        <span class="d-block">Draft</span>
-                                        <span class="fs-20 fw-bold d-block">3/10</span>
-                                    </a>
-                                    <div class="badge bg-soft-danger text-danger">
-                                        <i class="feather-arrow-down fs-10 me-1"></i>
-                                        <span>12.68%</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
         <!-- [ page-header ] end -->
@@ -194,56 +21,15 @@
                 <div class="col-lg-12">
                     <div class="card invoice-container">
                         <div class="card-header">
-                            <div>
-                                <h2 class="fs-16 fw-700 text-truncate-1-line mb-0 mb-sm-1">Invoice Preview</h2>
-                                <div class="dropdown d-none d-sm-block">
-                                    <a href="javascript:void(0)" class="dropdown-toggle d-flex align-items-center"
-                                        data-bs-toggle="dropdown" data-bs-offset="0,25" aria-expanded="false">
-                                        <span class="fs-11 fw-400 text-muted me-2">Invoice Templates</span>
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li>
-                                            <a href="javascript:void(0);" class="dropdown-item active">Default</a>
-                                        </li>
-                                        <li>
-                                            <a href="javascript:void(0);" class="dropdown-item">Simple</a>
-                                        </li>
-                                        <li>
-                                            <a href="javascript:void(0);" class="dropdown-item">Classic</a>
-                                        </li>
-                                        <li>
-                                            <a href="javascript:void(0);" class="dropdown-item">Modern</a>
-                                        </li>
-                                        <li>
-                                            <a href="javascript:void(0);" class="dropdown-item">Untimate</a>
-                                        </li>
-                                        <li>
-                                            <a href="javascript:void(0);" class="dropdown-item">Essential</a>
-                                        </li>
-                                        <li class="dropdown-divider"></li>
-                                        <li>
-                                            <a href="javascript:void(0);" class="dropdown-item">Create Template</a>
-                                        </li>
-                                        <li>
-                                            <a href="javascript:void(0);" class="dropdown-item">Delete Template</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
                             <div class="d-flex align-items-center justify-content-center">
-
                                 <a href="javascript:void(0)" class="d-flex me-1 printBTN">
-                                    <div class="avatar-text avatar-md" data-bs-toggle="tooltip"
-                                        data-bs-trigger="hover" title="Print Invoice"><i
-                                            class="feather feather-printer"></i></div>
+                                    <div class="avatar-text avatar-md" data-bs-toggle="tooltip" data-bs-trigger="hover"
+                                        title="Print Invoice"><i class="feather feather-printer"></i></div>
                                 </a>
-
                                 <a href="javascript:void(0)" class="d-flex me-1 file-download">
-                                    <div class="avatar-text avatar-md" data-bs-toggle="tooltip"
-                                        data-bs-trigger="hover" title="Download Invoice"><i
-                                            class="feather feather-download"></i></div>
+                                    <div class="avatar-text avatar-md" data-bs-toggle="tooltip" data-bs-trigger="hover"
+                                        title="Download Invoice"><i class="feather feather-download"></i></div>
                                 </a>
-
                             </div>
                         </div>
                         <div class="card-body p-0">
@@ -468,4 +254,4 @@
     </div>
     <!-- [ Main Content ] end -->
 </main>
-@include('layouts.partials.student.theme')
+@include('layouts.partials.admin.theme')
