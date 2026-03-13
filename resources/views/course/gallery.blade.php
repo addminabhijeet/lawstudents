441895c59e3e6ca2bb5aa2046e7e0fc3f6ce3dbd
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-03-13 21:45:06 +0530

    m

--- resources/views/course/gallery.blade.php
+++ resources/views/course/gallery.blade.php
@@ -10,67 +10,50 @@
         <div class="page-header">
             <div class="page-header-left d-flex align-items-center">
                 <div class="page-header-title">
-                    <h5 class="m-b-10">Proposal</h5>
+                    <h5 class="m-b-10">Admin</h5>
                 </div>
                 <ul class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                    <li class="breadcrumb-item">Create</li>
+                    <li class="breadcrumb-item">Banner</li>
+                    <li class="breadcrumb-item">Update</li>
                 </ul>
             </div>
-            <div class="page-header-right ms-auto">
-                <div class="page-header-right-items">
-                    <div class="d-flex d-md-none">
-                        <a href="javascript:void(0)" class="page-header-right-close-toggle">
-                            <i class="feather-arrow-left me-2"></i>
-                            <span>Back</span>
-                        </a>
-                    </div>
-                    <div class="d-flex align-items-center gap-2 page-header-right-items-wrapper">
-                        <a href="javascript:void(0);" class="btn btn-light-brand" data-bs-toggle="offcanvas"
-                            data-bs-target="#proposalSent">
-                            <i class="feather-layers me-2"></i>
-                            <span>Save & Send</span>
-                        </a>
-                        <a href="javascript:void(0);" class="btn btn-primary successAlertMessage">
-                            <i class="feather-save me-2"></i>
-                            <span>Save</span>
-                        </a>
-                    </div>
-                </div>
-                <div class="d-md-none d-flex align-items-center">
-                    <a href="javascript:void(0)" class="page-header-right-open-toggle">
-                        <i class="feather-align-right fs-20"></i>
-                    </a>
-                </div>
-            </div>
         </div>
-        <div class="card shadow-sm border-0">
-            <div class="card-header text-white">
-                <h5 class="mb-0">Gallery Settings</h5>
-            </div>
+        <div class="main-content">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card stretch stretch-full shadow-sm border-0">
 
-            <div class="card-body">
-                <form action="{{ route('admin.storegallery') }}" method="POST" enctype="multipart/form-data">
-                    @csrf
+                        <div class="card-header text-white">
+                            <h5 class="mb-0">Banner Settings</h5>
+                        </div>
 
-                    <input type="hidden" name="gallery_id" value="{{ $gallery->id ?? '' }}">
+                        <div class="card-body">
+                            <form action="{{ route('admin.storegallery') }}" method="POST"
+                                enctype="multipart/form-data">
+                                @csrf
 
-                    <div class="mb-3">
-                        <label class="form-label">Gallery Image</label>
-                        <input type="file" name="image_1" class="form-control">
+                                <input type="hidden" name="gallery_id" value="{{ $gallery->id ?? '' }}">
 
-                        @if ($gallery && $gallery->image)
-                            <div class="mt-2">
-                                <img src="{{ asset('storage/' . $gallery->image_1) }}" width="150"
-                                    class="img-thumbnail">
-                            </div>
-                        @endif
+                                <div class="mb-3">
+                                    <label class="form-label">Gallery Image</label>
+                                    <input type="file" name="image_1" class="form-control">
+
+                                    @if ($gallery && $gallery->image)
+                                        <div class="mt-2">
+                                            <img src="{{ asset('storage/' . $gallery->image_1) }}" width="150"
+                                                class="img-thumbnail">
+                                        </div>
+                                    @endif
+                                </div>
+
+                                <button type="submit" class="btn btn-primary">
+                                    Save Gallery
+                                </button>
+                            </form>
+                        </div>
+
                     </div>
-
-                    <button type="submit" class="btn btn-primary">
-                        Save Gallery
-                    </button>
-                </form>
+                </div>
             </div>
         </div>
     </div>
