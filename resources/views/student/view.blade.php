d7a744488cda0bbed6d1af8c5e4b351d993c731f
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-03-16 16:09:33 +0530

    m

--- resources/views/student/view.blade.php
+++ resources/views/student/view.blade.php
@@ -1,4 +1,4 @@
-@include('layouts.partials.admin.dashboard')
+@include('layouts.partials.student.dashboard')
 <main class="nxl-container">
     <!-- main containts -->
     <div class="nxl-content">
@@ -141,4 +141,4 @@
         });
     </script>
 </main>
-@include('layouts.partials.admin.theme')
+@include('layouts.partials.student.theme')
