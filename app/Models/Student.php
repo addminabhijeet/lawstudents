be4046c7ee9d164ff518876101ea25d6d3c113b3
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-03-16 21:09:08 +0530

    m

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -77,8 +77,4 @@ class Student extends Authenticatable
         return $this->hasMany(Payment::class, 'student_id');
     }
 
-    public function admission()
-    {
-        return $this->hasOne(StudentAdmission::class, 'student_id');
-    }
 }
