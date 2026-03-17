7a9b59a2db03ac83dd76db2978ad2f00ee561e6c
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-03-17 16:26:34 +0530

    m

--- resources/views/course/banner.blade.php
+++ resources/views/course/banner.blade.php
@@ -37,7 +37,7 @@
                                 <div class="row">
 
                                     <div class="col-md-4 mb-3">
-                                        <label class="form-label">Banner Image 1 (1920 x 1000)</label>
+                                        <label class="form-label">Banner Image 1 (1425 X 600)</label>
                                         <input type="file" name="image_1" class="form-control">
 
                                         @if ($banner && $banner->image_1)
@@ -49,7 +49,7 @@
                                     </div>
 
                                     <div class="col-md-4 mb-3">
-                                        <label class="form-label">Banner Image 2 (1920 x 1000)</label>
+                                        <label class="form-label">Banner Image 2 (1425 X 600)</label>
                                         <input type="file" name="image_2" class="form-control">
 
                                         @if ($banner && $banner->image_2)
@@ -61,7 +61,7 @@
                                     </div>
 
                                     <div class="col-md-4 mb-3">
-                                        <label class="form-label">Banner Image 3 (1920 x 1000)</label>
+                                        <label class="form-label">Banner Image 3 (1425 X 600)</label>
                                         <input type="file" name="image_3" class="form-control">
 
                                         @if ($banner && $banner->image_3)
