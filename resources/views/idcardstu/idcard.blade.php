8b90217b351e7a0d600a8d365ec5f5933f2a9e9e
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-03-17 22:01:45 +0530

    m

--- resources/views/idcardstu/idcard.blade.php
+++ resources/views/idcardstu/idcard.blade.php
@@ -109,25 +109,6 @@
 
                                 </div>
 
-                                <div class="col-7 d-flex justify-content-center">
-
-                                    <div style="font-size:10px;font-weight:600;">
-                                        @php
-                                            $statusColor = match ($idcard->payment_status) {
-                                                'paid' => 'bg-success',
-                                                'failed' => 'bg-danger',
-                                                'cancelled' => 'bg-secondary',
-                                                default => 'bg-warning',
-                                            };
-                                        @endphp
-
-                                        <span class="badge {{ $statusColor }}" style="font-size:9px;">
-                                            {{ ucfirst($idcard->payment_status) }}
-                                        </span>
-                                    </div>
-
-                                </div>
-
                             </div>
 
 
