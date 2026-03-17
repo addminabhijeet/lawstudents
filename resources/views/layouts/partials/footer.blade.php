2c6a8d6327fd7a57816262399148d055b78af9df
Addmin Abhijeet <addminabhijeet@gmail.com>
2026-03-17 20:03:28 +0530

    m

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -53,6 +53,7 @@
                                 <ul>
                                     <li><a href="{{ route('frontend.about') }}">About Us</a></li>
                                     <li><a href="{{ route('frontend.notes') }}">Free Notes</a></li>
+                                    <li><a href="{{ route('frontend.clientele') }}">Clientele</a></li>
                                     <li><a href="{{ route('frontend.gallery') }}">Gallery</a></li>
                                     <li><a href="{{ route('frontend.contact') }}">Contact Us</a></li>
                                     <li><a href="{{ route('login') }}">Login</a></li>
