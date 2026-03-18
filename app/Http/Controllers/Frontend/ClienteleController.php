cc3b0ded5d04a0400640357620973d9eefc05136
addminabhijeet <addmin.abhijeet@gmail.com>
2026-03-19 03:37:51 +0530

    m

--- app/Http/Controllers/Frontend/ClienteleController.php
+++ app/Http/Controllers/Frontend/ClienteleController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Clientele;
 use Illuminate\View\View;
 
 class ClienteleController extends Controller
 {
     public function __invoke(): View
     {
-        return view('clientele.clientele');
+        $clienteles = Clientele::all();
+
+        return view('clientele.clientele', compact('clienteles'));
     }
 }
