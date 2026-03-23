70a78133fb561de153df8e1bb3cfdc7c2e469787
addminabhijeet <addmin.abhijeet@gmail.com>
2026-03-23 20:44:16 +0530

    m

--- routes/admin.php
+++ routes/admin.php
@@ -118,7 +118,7 @@ Route::middleware(['admin.auth'])->group(function () {
             Route::post('store-banner', [CourseController::class, 'storebanner'])
                 ->name('storebanner');
 
-            Route::post('course-edit/{id}', [CourseController::class, 'editcourse'])
+            Route::get('course-edit/{id}', [CourseController::class, 'editcourse'])
                 ->name('editcourse');
 
             Route::post('course-update/{id}', [CourseController::class, 'updatecourse'])
