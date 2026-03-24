f6b6742db438aefa8be8fb5fc1816c3fa778b03f
addminabhijeet <addmin.abhijeet@gmail.com>
2026-03-24 20:28:36 +0530

    m

--- resources/views/student/list.blade.php
+++ resources/views/student/list.blade.php
@@ -36,16 +36,7 @@
                                 <table class="table table-hover" id="paymentList">
                                     <thead>
                                         <tr>
-                                            <th class="wd-30">
-                                                <div class="btn-group mb-1">
-                                                    <div class="custom-control custom-checkbox ms-1">
-                                                        <input type="checkbox" class="custom-control-input"
-                                                            id="checkAllPayment">
-                                                        <label class="custom-control-label"
-                                                            for="checkAllPayment"></label>
-                                                    </div>
-                                                </div>
-                                            </th>
+                                            
                                             <th>Username</th>
                                             <th>Email</th>
                                             <th>Name</th>
@@ -56,17 +47,7 @@
                                     <tbody>
                                         @foreach ($students as $student)
                                             <tr class="single-item">
-                                                <td>
-                                                    <div class="item-checkbox ms-1">
-                                                        <div class="custom-control custom-checkbox">
-                                                            <input type="checkbox"
-                                                                class="custom-control-input checkbox"
-                                                                id="checkBox_{{ $loop->index + 1 }}">
-                                                            <label class="custom-control-label"
-                                                                for="checkBox_{{ $loop->index + 1 }}"></label>
-                                                        </div>
-                                                    </div>
-                                                </td>
+                                                
                                                 <td><a href="javascript:void(0)"
                                                         class="fw-bold">{{ $student->username }}</a></td>
                                                 <td>
