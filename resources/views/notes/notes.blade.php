5f7e757a0b110ded90fffe1222171a5cb0ce5655
addminabhijeet <addmin.abhijeet@gmail.com>
2026-03-25 21:39:56 +0530

    m

--- resources/views/notes/notes.blade.php
+++ resources/views/notes/notes.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.landing', ['title' => 'Law Students || Criminal Law || Free Notes'])
+@extends('layouts.landing', ['title' => 'Law Students'])
 
 @section('content')
     <!--===== WELCOME STARTS =======-->
