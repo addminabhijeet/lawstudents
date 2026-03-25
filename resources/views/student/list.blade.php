427c543791bbd5d4c5fcf095da1af16db90af9f3
addminabhijeet <addmin.abhijeet@gmail.com>
2026-03-26 02:32:42 +0530

    m

--- resources/views/student/list.blade.php
+++ resources/views/student/list.blade.php
@@ -33,6 +33,30 @@
                     <div class="card stretch stretch-full">
                         <div class="card-body p-0">
                             <div class="table-responsive">
+                                @if ($errors->any())
+                                    <div class="alert alert-danger alert-dismissible fade show">
+                                        <ul class="mb-0">
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                                    </div>
+                                @endif
+
+                                @if (session('error'))
+                                    <div class="alert alert-danger alert-dismissible fade show">
+                                        {{ session('error') }}
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                                    </div>
+                                @endif
+
+                                @if (session('success'))
+                                    <div class="alert alert-success alert-dismissible fade show">
+                                        {{ session('success') }}
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                                    </div>
+                                @endif
                                 <table class="table table-hover" id="paymentList">
                                     <thead>
                                         <tr>
@@ -78,7 +102,8 @@
                                                             <i class="feather feather-edit"></i>
                                                         </a>
 
-                                                        <form action="{{ route('admin.destroystudent', $student->id) }}"
+                                                        <form
+                                                            action="{{ route('admin.destroystudent', $student->id) }}"
                                                             method="POST"
                                                             onsubmit="return confirm('Are you sure you want to delete this student?');">
                                                             @csrf
