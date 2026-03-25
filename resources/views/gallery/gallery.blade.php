d2f537a543fbb347b72f06ed51cebc621c3fc95e
addminabhijeet <addmin.abhijeet@gmail.com>
2026-03-25 21:42:03 +0530

    m

--- resources/views/gallery/gallery.blade.php
+++ resources/views/gallery/gallery.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.landing', ['title' => 'Law Students || Criminal Law || Our Team'])
+@extends('layouts.landing', ['title' => 'Law Students'])
 
 @section('content')
     <!--===== WELCOME STARTS =======-->
