414953494af7d8dc22690247c3c96fd3ea78bb3c
addminabhijeet <addmin.abhijeet@gmail.com>
2026-04-02 04:16:53 +0530

    m

--- resources/views/payment/view.blade.php
+++ resources/views/payment/view.blade.php
@@ -56,19 +56,21 @@ $user = \App\Models\User::first();
 
                                     <!-- LEFT: centerone -->
                                     <div class="col-12 col-sm-4 mb-3 mb-sm-0">
-                                        <address class="text-muted small mb-0 lh-lg">
-                                            @if (!empty($user?->centerone))
-                                            {!! collect(explode(' ', trim($user->centerone)))
-                                            ->chunk(3)
-                                            ->map(fn($chunk) => implode(' ', $chunk->toArray()))
-                                            ->implode('<br>') !!}
-                                            <br>
-                                            @else
-                                            P.O. Box 18728,<br>
-                                            DeLorean New York<br>
-                                            VAT No: 2617 348 2752<br>
-                                            @endif
-                                        </address>
+                                        <strong>
+                                            <address class="text-muted small mb-0 lh-lg">
+                                                @if (!empty($user?->centerone))
+                                                {!! collect(explode(' ', trim($user->centerone)))
+                                                ->chunk(3)
+                                                ->map(fn($chunk) => implode(' ', $chunk->toArray()))
+                                                ->implode('<br>') !!}
+                                                <br>
+                                                @else
+                                                P.O. Box 18728,<br>
+                                                DeLorean New York<br>
+                                                VAT No: 2617 348 2752<br>
+                                                @endif
+                                            </address>
+                                        </strong>
                                     </div>
 
                                     <!-- CENTER: Logo -->
@@ -85,19 +87,21 @@ $user = \App\Models\User::first();
 
                                     <!-- RIGHT: centertwo -->
                                     <div class="col-12 col-sm-4 text-sm-end">
-                                        <address class="text-muted small mb-0 lh-lg">
-                                            @if (!empty($user?->centertwo))
-                                            {!! collect(explode(' ', trim($user->centertwo)))
-                                            ->chunk(3)
-                                            ->map(fn($chunk) => implode(' ', $chunk->toArray()))
-                                            ->implode('<br>') !!}
-                                            <br>
-                                            @else
-                                            P.O. Box 18728,<br>
-                                            DeLorean New York<br>
-                                            VAT No: 2617 348 2752<br>
-                                            @endif
-                                        </address>
+                                        <strong>
+                                            <address class="text-muted small mb-0 lh-lg">
+                                                @if (!empty($user?->centertwo))
+                                                {!! collect(explode(' ', trim($user->centertwo)))
+                                                ->chunk(3)
+                                                ->map(fn($chunk) => implode(' ', $chunk->toArray()))
+                                                ->implode('<br>') !!}
+                                                <br>
+                                                @else
+                                                P.O. Box 18728,<br>
+                                                DeLorean New York<br>
+                                                VAT No: 2617 348 2752<br>
+                                                @endif
+                                            </address>
+                                        </strong>
                                     </div>
 
                                 </div>
