ce713d041db2da6654155afabc30079ea419d78c
addminabhijeet <addmin.abhijeet@gmail.com>
2026-04-02 03:15:41 +0530

    m

--- resources/views/payment/view.blade.php
+++ resources/views/payment/view.blade.php
@@ -56,7 +56,7 @@ $user = \App\Models\User::first();
 
                                     <!-- LEFT: centerone -->
                                     <div class="col-12 col-sm-4 mb-3 mb-sm-0">
-                                        <address class="text-muted small mb-0 lh-lg">
+                                        <address class="text-muted fw-bold small mb-0 lh-lg">
                                             @if (!empty($user?->centerone))
                                             {!! collect(explode(' ', trim($user->centerone)))
                                             ->chunk(3)
