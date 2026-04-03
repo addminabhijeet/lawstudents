0692362abf1177afe53bad298eba252422910d5e
addminabhijeet <addmin.abhijeet@gmail.com>
2026-04-03 16:55:56 +0530

    mm

--- resources/views/rules/categories/edit.blade.php
+++ resources/views/rules/categories/edit.blade.php
@@ -26,7 +26,7 @@
                 <div class="col-lg-12">
                     <div class="card stretch stretch-full">
                         <div class="card-body">
-                            <form action="{{ route('admin.updaterulescategory', $categorie->id) }}" method="POST">
+                            <form action="{{ route('admin.updaterulescategory', $categories->id) }}" method="POST">
                                 @csrf
                                 @method('PUT')
                                 <div class="mb-3">
