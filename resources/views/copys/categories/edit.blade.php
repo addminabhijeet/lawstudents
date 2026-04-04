6be68f336b071917475170c193c4330bb392476f
addminabhijeet <addmin.abhijeet@gmail.com>
2026-04-04 13:18:08 +0530

    mm

--- resources/views/copys/categories/edit.blade.php
+++ resources/views/copys/categories/edit.blade.php
@@ -26,7 +26,7 @@
                 <div class="col-lg-12">
                     <div class="card stretch stretch-full">
                         <div class="card-body">
-                            <form action="{{ route('admin.updatecopycategory', $categories->id) }}" method="POST">
+                            <form action="{{ route('admin.updatecopyscategory', $categories->id) }}" method="POST">
                                 @csrf
                                
                                 <div class="mb-3">
