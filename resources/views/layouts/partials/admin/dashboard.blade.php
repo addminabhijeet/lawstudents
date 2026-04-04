32c898dc787ffaad8a91bbc8ba7ba150f602e825
addminabhijeet <addmin.abhijeet@gmail.com>
2026-04-04 15:03:25 +0530

    mm

--- resources/views/layouts/partials/admin/dashboard.blade.php
+++ resources/views/layouts/partials/admin/dashboard.blade.php
@@ -119,15 +119,6 @@
                             <li class="nxl-item"><a class="nxl-link" href="{{ route('admin.listcourse') }}">List Courses</a>
                             </li>
                         </ul>
-                    </li>
-
-                    <!-- Notes -->
-                    <li class="nxl-item nxl-hasmenu">
-                        <a href="javascript:void(0);" class="nxl-link">
-                            <span class="nxl-micon"><i class="feather-edit"></i></span>
-                            <span class="nxl-mtext">Notes</span>
-                            <span class="nxl-arrow"><i class="feather-chevron-right"></i></span>
-                        </a>
                         <ul class="nxl-submenu">
                             <li class="nxl-item"><a class="nxl-link" href="{{ route('admin.listnotes') }}">Notes</a>
                             </li>
