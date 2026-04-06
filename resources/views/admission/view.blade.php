c410aaed86e29b34478d146d39cf78c54026af67
addminabhijeet <addmin.abhijeet@gmail.com>
2026-04-06 20:35:23 +0530

    mm

--- resources/views/admission/view.blade.php
+++ resources/views/admission/view.blade.php
@@ -360,10 +360,10 @@ $user = \App\Models\User::first();
                                             @if ($admission->photo)
                                             <img src="{{ asset('storage/app/public/' . $admission->photo) }}"
                                                 class="rounded-circle mb-3"
-                                                style="width:100px; height:100px; object-fit:cover;">
+                                                style="width:90px; height:90px; object-fit:cover;">
                                             @else
                                             <img src="{{ asset('images/default-user.png') }}" class="rounded-circle mb-3"
-                                                style="width:100px; height:100px;">
+                                                style="width:90px; height:90px;">
                                             @endif
 
                                             <address class="text-muted lh-sm">
