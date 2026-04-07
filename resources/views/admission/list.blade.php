2325394f7c83706afecf6a8a467aa4ea0de15117
addminabhijeet <addmin.abhijeet@gmail.com>
2026-04-08 02:43:51 +0530

    mm

--- resources/views/admission/list.blade.php
+++ resources/views/admission/list.blade.php
@@ -126,9 +126,7 @@
                                         @endforeach
                                         @if ($admissions->isEmpty())
                                             <tr>
-                                                <td colspan="8" class="text-center text-muted">No admissions
-                                                    admissions
-                                                    found.
+                                                <td colspan="8" class="text-center text-muted">No admissions found.
                                                 </td>
                                             </tr>
                                         @endif
