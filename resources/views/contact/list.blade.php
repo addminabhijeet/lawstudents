91dbb80f3e3bef7562bfacce4844b20d4b90a9f1
addminabhijeet <addmin.abhijeet@gmail.com>
2026-04-08 17:39:10 +0530

    mm

--- resources/views/contact/list.blade.php
+++ resources/views/contact/list.blade.php
@@ -51,7 +51,7 @@
                                                 <div class="hstack gap-2 justify-content-end">
 
                                                     <!-- MAIL BUTTON -->
-                                                    <a href="{{ route('contact.sendmail', $item->id) }}"
+                                                    <a href=""
                                                         class="btn btn-sm btn-success">
                                                         Mail
                                                     </a>
